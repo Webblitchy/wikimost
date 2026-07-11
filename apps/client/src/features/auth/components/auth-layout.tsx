752583cef623e20f9d945873c65fc1917ad8ae51
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2026-07-11 11:43:27 +0200

    fix login icon color

--- apps/client/src/features/auth/components/auth-layout.tsx
+++ apps/client/src/features/auth/components/auth-layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Group, Text } from "@mantine/core";
+import { Group, Text, useComputedColorScheme } from "@mantine/core";
 import classes from "./auth.module.css";
 
 type AuthLayoutProps = {
@@ -7,14 +7,23 @@ type AuthLayoutProps = {
 };
 
 export function AuthLayout({ children }: AuthLayoutProps) {
+  // Invert favicon color to be visible
+  const colorScheme = useComputedColorScheme();
+  let useFilter = 1;
+  if (colorScheme === "dark") {
+    useFilter = 0;
+  }
   return (
     <>
       <Group justify="center" gap={8} className={classes.logo}>
         <img
-          src="/icons/favicon-32x32.png"
+          src="/favicon-32x32.png"
           alt="Wikimost"
           width={22}
           height={22}
+          style={{
+            filter:`invert(${useFilter})`,
+          }}
         />
         <Text size="28px" fw={700} style={{ userSelect: "none" }}>
           Wikimost
